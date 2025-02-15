bf304b01953c98531d31d5ca6b34850dbef91875
YasmeenGad <gadyasmeen335@gmail.com>
2025-02-15 03:21:14 +0200

    feat: impl verify otp repo

--- lib/features/auth/data/repositories/auth_repo_impl.dart
+++ lib/features/auth/data/repositories/auth_repo_impl.dart
@@ -2,6 +2,7 @@ import 'package:fitness_app/core/networking/common/api_result.dart';
 import 'package:fitness_app/features/auth/data/data_sources/contracts/online_data_sources/auth_online_data_source.dart';
 import 'package:fitness_app/features/auth/domain/contracts/auth_repo.dart';
 import 'package:fitness_app/features/auth/domain/entities/request/forget_password_request_entity.dart';
+import 'package:fitness_app/features/auth/domain/entities/request/verify_otp_request_enity.dart';
 import 'package:fitness_app/features/auth/domain/entities/response/forget_password_response_entity.dart';
 import 'package:injectable/injectable.dart';
 
@@ -15,5 +16,10 @@ class AuthRepoImpl implements AuthRepo{
   @override
   Future<DataResult<ForgetPasswordResponseEntity>> forgetPassword(ForgetPasswordRequestEntity request) async{
     return await _onlineDataSource.forgetPassword(request);
+  }
+
+  @override
+  Future<DataResult<void>> verifyOtp(VerifyOtpRequestEntity request) async {
+    return await _onlineDataSource.verifyOtp(request);
   }
 }
