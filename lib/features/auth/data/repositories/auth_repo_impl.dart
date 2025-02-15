ef344628cd8a4c155ca9af535527e42ff5d0574c
YasmeenGad <gadyasmeen335@gmail.com>
2025-02-15 05:19:23 +0200

    feat: impl reset password repo

--- lib/features/auth/data/repositories/auth_repo_impl.dart
+++ lib/features/auth/data/repositories/auth_repo_impl.dart
@@ -2,8 +2,10 @@ import 'package:fitness_app/core/networking/common/api_result.dart';
 import 'package:fitness_app/features/auth/data/data_sources/contracts/online_data_sources/auth_online_data_source.dart';
 import 'package:fitness_app/features/auth/domain/contracts/auth_repo.dart';
 import 'package:fitness_app/features/auth/domain/entities/request/forget_password_request_entity.dart';
+import 'package:fitness_app/features/auth/domain/entities/request/reset_password_request_entity.dart';
 import 'package:fitness_app/features/auth/domain/entities/request/verify_otp_request_enity.dart';
 import 'package:fitness_app/features/auth/domain/entities/response/forget_password_response_entity.dart';
+import 'package:fitness_app/features/auth/domain/entities/response/reset_password_response_entity.dart';
 import 'package:injectable/injectable.dart';
 
 @Injectable(as: AuthRepo)
@@ -21,5 +23,11 @@ class AuthRepoImpl implements AuthRepo{
   @override
   Future<DataResult<void>> verifyOtp(VerifyOtpRequestEntity request) async {
     return await _onlineDataSource.verifyOtp(request);
+  }
+
+  @override
+  Future<DataResult<ResetPasswordResponseEntity>> resetPassword(
+      ResetPasswordRequestEntity request) async {
+    return await _onlineDataSource.resetPassword(request);
   }
 }
