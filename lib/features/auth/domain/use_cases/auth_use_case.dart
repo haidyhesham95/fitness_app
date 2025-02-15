cc8ee396689979971a5f30d4fff07b319f66d6a8
YasmeenGad <gadyasmeen335@gmail.com>
2025-02-15 05:20:22 +0200

    feat: impl reset password use case

--- lib/features/auth/domain/use_cases/auth_use_case.dart
+++ lib/features/auth/domain/use_cases/auth_use_case.dart
@@ -3,8 +3,10 @@ import 'package:injectable/injectable.dart';
 
 import '../../../../core/networking/common/api_result.dart';
 import '../contracts/auth_repo.dart';
+import '../entities/request/reset_password_request_entity.dart';
 import '../entities/request/verify_otp_request_enity.dart';
 import '../entities/response/forget_password_response_entity.dart';
+import '../entities/response/reset_password_response_entity.dart';
 
 @injectable
 class AuthUseCase {
@@ -12,8 +14,14 @@ class AuthUseCase {
 
   AuthUseCase(this._authRepo);
 
-  Future<DataResult<ForgetPasswordResponseEntity>> forgetPassword(ForgetPasswordRequestEntity request) async => await _authRepo.forgetPassword(request);
+  Future<DataResult<ForgetPasswordResponseEntity>> forgetPassword(
+          ForgetPasswordRequestEntity request) async =>
+      await _authRepo.forgetPassword(request);
 
   Future<DataResult<void>> verifyOtp(VerifyOtpRequestEntity request) async =>
       await _authRepo.verifyOtp(request);
+
+  Future<DataResult<ResetPasswordResponseEntity>> resetPassword(
+          ResetPasswordRequestEntity request) async =>
+      await _authRepo.resetPassword(request);
 }
